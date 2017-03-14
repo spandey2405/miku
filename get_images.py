d95d400ab90a1fdfcb2f52110a2548084c2cc929
sid <scoder91@gmail.com>
2017-03-14 19:24:55 +0530

    First Push

--- get_images.py
+++ get_images.py
@@ -2,9 +2,14 @@ import os, json, urllib
 BASE_DIR = "gifinfo/"
 path, dirs, files = os.walk(BASE_DIR).next()
 
+paths_gif, dirs_gid, files_gif = os.walk("gifs/").next()
+
 for file in files:
     with open("gifinfo/" + file) as data_file:
         data = json.load(data_file)
     image_name = "gifs/" + data['id'] + ".gif"
-    urllib.urlretrieve(data['link'], image_name)
-    print image_name
+    if (data['id'] + ".gif") not in files_gif:
+        urllib.urlretrieve(data['link'], image_name)
+        print image_name
+    else:
+        print "skipped", image_name
