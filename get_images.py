7cc984421c1c0e4246648b2984ed532e22f72b4f
sid <scoder91@gmail.com>
2017-03-14 19:02:13 +0530

    First Push

--- get_images.py
+++ get_images.py
@@ -1,4 +1,10 @@
-import os
+import os, json
 BASE_DIR = "gifinfo/"
 path, dirs, files = os.walk(BASE_DIR).next()
 
+for file in files:
+    with open(file) as data_file:
+        data = json.load(data_file)
+    image_name = "gifs/" + element['id'] + ".gif"
+    urllib.urlretrieve(file['link'], image_name)
+    print image_name
